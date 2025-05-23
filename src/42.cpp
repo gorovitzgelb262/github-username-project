85b65eb389a64099087458201f1be42b245793d8
gorovitzgelb262 <gorovitzgelb262@gmail.com>
2025-05-23 07:17:31 +0000

    Add 42.cpp

---
+++ src/42.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    double y = 3.14;
+    string z = "Hello, World!";
+    cout << "Value of x: " << x << endl;
+    cout << "Value of y: " << y << endl;
+    cout << "String 'z': " << z << endl;
+    return 0;
+}
